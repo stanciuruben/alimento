3ef6105193dc88e94c83c037a5adc8765ba964ee
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-02-01 18:27:52 +0200

    eslint configuration

--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
-import express, { Express } from 'express';
+/* eslint-disable @typescript-eslint/no-var-requires */
+import express, { type Express } from 'express';
+import cookieParser from 'cookie-parser';
 const app: Express = express();
-const cookieParser = require('cookie-parser');
 
-// @ts-ignore next line
+// @ts-expect-error next line
 app.use(express.json({ extended: false }));
 app.use(cookieParser());
 
-//Routes
+// Routes
 app.use('/suggestions', require('./routes/suggestions'));
 
 const PORT = 9999;
 app.listen(PORT, function () {
-   console.log('\x1b[36m', `Listening at http://localhost:${PORT}`);
-   console.log('\x1b[0m', '');
+  console.log('\x1b[36m', `Listening at http://localhost:${PORT}`);
+  console.log('\x1b[0m', '');
 });
--- routes/suggestions.ts
+++ routes/suggestions.ts
@@ -1,23 +1,30 @@
-import express, { Request, Response } from 'express';
-const { body } = require('express-validator');
-const router = express.Router();
-const config = require('config');
-const { Configuration, OpenAIApi } = require("openai");
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import express, { type Request, type Response } from 'express';
+import { body } from 'express-validator';
+import config from 'config';
+import { Configuration, OpenAIApi } from 'openai';
 const configuration = new Configuration({
-    apiKey: config.get('OPENAI_AUTH-KEY'),
+    apiKey: config.get('OPENAI_AUTH-KEY')
 });
 const openai = new OpenAIApi(configuration);
+const router = express.Router();
 
 // @route   GET
 // @desc    Given parameters make a request to the openai api
 // @desc    and return the response to the client
 // @access  Private
-router.get('/', async (req: Request, res: Response) => {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: "give me a breakfast recipe with 50g of protein and 10g of net carbs",
-    });
-    res.send(response.data);
+router.get('/', async (_req: Request, res: Response) => {
+    try {
+        const response = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: 'breakfast suggestion with following macros: 50g protein, 50g fat, 10g carbs',
+            temperature: 1,
+            max_tokens: 150
+        });
+        res.send(response.data.choices);
+    } catch (error: any) {
+        res.send(error);
+    }
 });
 
 module.exports = router;
