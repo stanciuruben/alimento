491e753f97917827e77bca82b8236e9977e274be
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-02-02 11:22:32 +0200

    installed client framework

--- routes/suggestions.ts
+++ routes/suggestions.ts
@@ -1,12 +1,7 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import express, { type Request, type Response } from 'express';
 import { body } from 'express-validator';
-import config from 'config';
-import { Configuration, OpenAIApi } from 'openai';
-const configuration = new Configuration({
-    apiKey: config.get('OPENAI_AUTH-KEY')
-});
-const openai = new OpenAIApi(configuration);
+import aiRequest from '../services/aiRequest';
 const router = express.Router();
 
 // @route   GET
@@ -15,13 +10,8 @@ const router = express.Router();
 // @access  Private
 router.get('/', async (_req: Request, res: Response) => {
     try {
-        const response = await openai.createCompletion({
-            model: 'text-davinci-003',
-            prompt: 'breakfast suggestion with following macros: 50g protein, 50g fat, 10g carbs',
-            temperature: 1,
-            max_tokens: 150
-        });
-        res.send(response.data.choices);
+        const response = await aiRequest('list a meal plan with the following macros: 55g protein, 55g fat, 10g net carbs')
+        res.send(response);
     } catch (error: any) {
         res.send(error);
     }
---
+++ services/aiRequest.ts
@@ -0,0 +1,16 @@
+import config from 'config';
+import { Configuration, OpenAIApi } from 'openai';
+const configuration = new Configuration({
+    apiKey: config.get('OPENAI_AUTH-KEY')
+});
+const openai = new OpenAIApi(configuration);
+
+export default async (prompt: string): Promise<any> => {
+    const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt,
+        temperature: 0,
+        max_tokens: 150
+    });
+    return response.data.choices
+};
