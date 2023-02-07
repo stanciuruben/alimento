9270feac6373676d8a0e5fe683c61bfbf503e1b2
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-02-07 12:05:30 +0200

    feat: style for MoreOptions component

--- client/src/components/MoreOptions/MoreOptions.tsx
+++ client/src/components/MoreOptions/MoreOptions.tsx
@@ -1,37 +1,85 @@
 /* eslint-disable multiline-ternary */
-import { type FC, useState } from 'react';
+import { type FC, useState, type ChangeEvent } from 'react';
 // import './MoreOptions.css';
 
 interface Props {}
 
 const MoreOptions: FC<Props> = () => {
     const [useMacros, setUseMacros] = useState<boolean>(false);
+    const [kcal, setKcal] = useState<number>(2000);
+
+    function changeKcal (e: ChangeEvent<HTMLInputElement>): void {
+        setKcal(parseInt(e.target.value));
+    }
+
     return (
         <div className="form__more_options">
-            <fieldset>
-                <h2>Use Macros?</h2>
+            <fieldset className="form__more_options__row">
+                <label htmlFor="kcal-macros" className="form_label">
+                    Use kilocalories/macronutrients:
+                </label>
                 <input
                     type="checkbox"
                     name="kcal-macros"
                     id="kcal-macros"
+                    className="form_checkbox"
                     onChange={() => {
                         setUseMacros(!useMacros);
                     }}
                 />
             </fieldset>
             {useMacros ? (
-                <fieldset>
-                    <label htmlFor="protein">Grams of Protein</label>
-                    <input type="number" name="protein" id="protein" />
-                    <label htmlFor="carbs">Grams of Carbohidrates</label>
-                    <input type="number" name="carbs" id="carbs" />
-                    <label htmlFor="fat">Grams of Fat</label>
-                    <input type="number" name="fat" id="fat" />
+                <fieldset className="form__more_options__row">
+                    <div className="form__more_options__row--1">
+                        <label htmlFor="protein" className="form_label">
+                            Proteins:
+                        </label>
+                        <input
+                            type="text"
+                            name="protein"
+                            id="protein"
+                            className="form_input--number"
+                        />
+                        g
+                    </div>
+                    <div className="form__more_options__row--1" >
+                        <label htmlFor="carbs" className="form_label">
+                            Carbohidrates:
+                        </label>
+                        <input
+                            type="text"
+                            name="carbs"
+                            id="carbs"
+                            className="form_input--number"
+                        />
+                        g
+                    </div>
+                    <div className="form__more_options__row--1" >
+                        <label htmlFor="fat" className="form_label">
+                            Fats:
+                        </label>
+                        <input
+                            type="text"
+                            name="fat"
+                            id="fat"
+                            className="form_input--number"
+                        />
+                        g
+                    </div>
                 </fieldset>
             ) : (
-                <fieldset>
-                    <label htmlFor="kcal">Kilocalories</label>
-                    <input type="number" name="kcal" id="kcal" />
+                <fieldset className="form__more_options__row">
+                    <label htmlFor="kcal" className="form_label">
+                        Kilocalories:
+                    </label>
+                    <input
+                        type="text"
+                        name="kcal"
+                        id="kcal"
+                        value={kcal}
+                        onChange={changeKcal}
+                        className="form_input--number"
+                    />
                 </fieldset>
             )}
         </div>
