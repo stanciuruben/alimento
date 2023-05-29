5e52970a024d22a266a4ac50be669819d3979e90
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-05-29 14:34:49 +0300

    feat: Singel MealPlan Display

---
+++ client/src/components/MealPlans/Single/RenameModal/RenameModal.tsx
@@ -0,0 +1,58 @@
+import { type FC, useState, type Dispatch } from 'react';
+
+import type MealPlanViewAction from '../../../../types/MealPlanViewAction';
+import MealPlan from '../../../../lib/requests/MealPlan';
+
+const RenameModal: FC<{
+    name: string
+    setMealPlansView: Dispatch<MealPlanViewAction>
+}> = ({
+    name,
+    setMealPlansView
+}) => {
+        const [nameInput, setNameInput] = useState<string>(name);
+
+        const onSubmit = async (): void => {
+            const res = await new MealPlan().update(nameInput);
+            setMealPlansView({ type: 'CHANGE_NAME', payload: { name: res.name } });
+        }
+
+        return (
+            <div
+                className="modal fade"
+                id="renameModal"
+                data-bs-backdrop="static"
+                data-bs-keyboard="false"
+                tabIndex={-1}
+                aria-labelledby="renameLabel"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="renameLabel">
+                                Rename Plan
+                            </h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <input
+                                value={nameInput}
+                                onChange={(e) => { setNameInput(e.target.value); }}
+                                type="text"
+                                name="allergens"
+                                id="allergens-input"
+                                className='form-control'
+                            />
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-light" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={onSubmit} >Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+export default RenameModal;
---
+++ client/src/components/MealPlans/Single/Single.tsx
@@ -0,0 +1,96 @@
+import { type FC, type Dispatch } from 'react';
+
+import type Options from '../../../types/Options';
+import type MealPlanViewAction from '../../../types/MealPlanViewAction';
+
+import RenameModal from './RenameModal/RenameModal';
+
+const SingleMealPlan: FC<{
+    options: Options
+    name: string
+    text: string
+    setMealPlansView: Dispatch<MealPlanViewAction>
+}> = ({
+    options,
+    name,
+    text,
+    setMealPlansView
+}) => {
+        return (
+            <div className="container">
+                <hgroup className='text-center my-5'>
+                    <h1 className='d-inline-block align-middle m-0 me-3'>
+                        {name}
+                    </h1>
+                    <button
+                        type='button'
+                        className='btn btn-light d-inline-block align-middle m-0'
+                        tabIndex={0}
+                        data-bs-toggle="modal"
+                        data-bs-target="#renameModal"
+                        title='Rename Plan'
+                    >
+                        <i className="bi bi-pencil-fill"></i>
+                    </button>
+                </hgroup>
+                <div className="d-flex my-5">
+                    <button
+                        className='col btn btn-secondary me-3'
+                        type='button'
+                        onClick={() => { setMealPlansView({ type: 'CHANGE_VIEW', payload: 'list' }); }}
+                    >
+                        Back To List
+                    </button>
+                    <button
+                        className='col btn btn-primary'
+                        type='button'
+                        onClick={() => { setMealPlansView({ type: 'CHANGE_VIEW', payload: 'form' }); }}
+                    >
+                        Generate Another
+                    </button>
+                </div>
+                <div className='my-5'>
+                    <h3 className='mb-3'>
+                        Selected Options
+                    </h3>
+                    {
+                        <ul className='list-group text-center'>
+                            <li className='list-group-item' ><b>Allergens:</b> {options.allergens.length > 0 ? options.allergens.join(', ') : '(none)'}.</li>
+                            {
+                                options.useMacros
+                                    ? <>
+                                        <li className='list-group-item' ><b>Protein:</b> {options.protein}</li>
+                                        <li className='list-group-item' ><b>Carbs:</b> {options.carbs}</li>
+                                        <li className='list-group-item' ><b>Fat:</b> {options.fat}</li>
+                                        <li className='list-group-item' ><b>Kcal:</b> {options.fat * 9 + (options.protein + options.carbs) * 4}</li>
+                                    </>
+                                    : <li className='list-group-item' ><b>Kcal:</b> {options.kcal}</li>
+                            }
+                        </ul>
+                    }
+                </div>
+                <div className='my-5' >
+                    <h3 className='mb-3'>
+                        Meal Plan
+                    </h3>
+                    <p>
+                        {
+                            text.split('\n').map((line, idx) => {
+                                return (
+                                    <>
+                                        <span key={'line-' + idx.toString()}>
+                                            {line}
+                                        </span>
+                                        <br />
+                                    </>
+                                )
+                            })
+                        }
+                    </p>
+                </div>
+                <RenameModal name={name} setMealPlansView={setMealPlansView} />
+            </div>
+        );
+    }
+
+export default SingleMealPlan;
