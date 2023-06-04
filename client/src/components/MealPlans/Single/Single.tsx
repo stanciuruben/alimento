78f7d8424c523d962786f254ed142ebe1621d9ef
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-06-04 15:52:30 +0300

    feat: Chart in single

--- client/src/components/MealPlans/Single/Single.tsx
+++ client/src/components/MealPlans/Single/Single.tsx
@@ -1,10 +1,11 @@
-import { type FC, type Dispatch } from 'react';
+import { type FC, type Dispatch, useMemo } from 'react';
 
 import type Options from '../../../types/Options';
 import type MealPlanViewAction from '../../../types/MealPlanViewAction';
 import capitalizeString from '../../../lib/utils/capitalizeString';
 
 import RenameModal from './RenameModal/RenameModal';
+import DoughnutChart from '../../Charts/DoughnutChart';
 
 const SingleMealPlan: FC<{
     id: number
@@ -19,6 +20,15 @@ const SingleMealPlan: FC<{
     text,
     setMealPlansView
 }) => {
+        const percetages = useMemo(() => {
+            const kcal = options.fat * 9 + (options.protein + options.carbs) * 4;
+            return {
+                p: (options.protein * 4 / kcal * 100).toPrecision(4),
+                c: (options.carbs * 4 / kcal * 100).toPrecision(4),
+                f: (options.fat * 9 / kcal * 100).toPrecision(4)
+            }
+        }, [options]);
+
         return (
             <div className="container">
                 <hgroup className='text-center my-5'>
@@ -52,18 +62,40 @@ const SingleMealPlan: FC<{
                         Generate Another
                     </button>
                 </div>
-                <div className='my-5'>
-                    <h3 className='mb-3'>
-                        Details
-                    </h3>
-                    <ul className='list-group'>
-                        <li className='list-group-item' ><b>Diet:</b> {capitalizeString(options.diet)}</li>
-                        <li className='list-group-item' ><b>Allergens:</b> {options.allergens.length > 0 ? options.allergens.join(', ') + '.' : '(none)'}</li>
-                        <li className='list-group-item' ><b>Protein:</b> {options.protein}</li>
-                        <li className='list-group-item' ><b>Carbs:</b> {options.carbs}</li>
-                        <li className='list-group-item' ><b>Fat:</b> {options.fat}</li>
-                        <li className='list-group-item' ><b>Kcal:</b> {options.fat * 9 + (options.protein + options.carbs) * 4}</li>
-                    </ul>
+                <div className='my-5 row'>
+                    <div className="col-lg-6">
+                        <h3 className='mb-3'>
+                            Details
+                        </h3>
+                        <ul className='list-group'>
+                            <li className='list-group-item' ><b>Diet:</b> {capitalizeString(options.diet)}</li>
+                            <li className='list-group-item' ><b>Allergens:</b> {options.allergens.length > 0 ? options.allergens.join(', ') + '.' : '(none)'}</li>
+                            <li className='list-group-item' ><b>Protein:</b> {options.protein}</li>
+                            <li className='list-group-item' ><b>Carbs:</b> {options.carbs}</li>
+                            <li className='list-group-item' ><b>Fat:</b> {options.fat}</li>
+                            <li className='list-group-item' ><b>Kcal:</b> {options.fat * 9 + (options.protein + options.carbs) * 4}</li>
+                        </ul>
+                    </div>
+                    <div className="col-lg-6 my-3">
+                        <DoughnutChart
+                            timeSpan={0}
+                            title='Macros to Kcal Ratio'
+                            id='macros-to-kcal-ratio'
+                            chartData={{
+                                labels: ['Protein', 'Carbohidrates', 'Fat'],
+                                datasets: [
+                                    {
+                                        label: 'Macros To Kcal in Percentage',
+                                        data: [
+                                            parseFloat(percetages.p),
+                                            parseFloat(percetages.c),
+                                            parseFloat(percetages.f)
+                                        ]
+                                    }
+                                ]
+                            }}
+                        />
+                    </div>
                 </div>
                 <div className='my-5' >
                     <h3 className='mb-3'>
