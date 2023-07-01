457e416b41e8ff3f0ef532704e22eabc785bbf67
Ruben Stanciu <stanciu.ruben20@gmail.com>
2023-07-01 15:10:30 +0200

    removed: unused line

--- routes/index.ts
+++ routes/index.ts
@@ -1,13 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import express from 'express';
-import path from 'path';
 
 const router = express.Router();
-
-// Homepage
-router.get('/', (_req, res) => {
-	res.sendFile(path.join(__dirname, '../homepage/index.html'));
-});
 
 router.use('/auth', require('./auth/index'));
 router.use('/mealplan', require('./mealplan/index'));
